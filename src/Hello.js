9428c3ad61d52b3e99c79382a39dbc0ec2d2cb4f
Freewind <nowindlee@gmail.com>
2024-01-30 23:26:59 +0800

    parcel no-cache

---
+++ src/Hello.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Hello = () => {
+    return React.createElement("div", null, "Hello from React");
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Hello } from './Hello';
+import ReactDOM from 'react-dom';
+ReactDOM.render(React.createElement(Hello, null), document.body);
